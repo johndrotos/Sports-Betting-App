51520671eef3e72ec833843b6159e286df324133
John Drotos <johndrotos3@gmail.com>
2025-01-12 17:39:11 -0500

    formatting

--- formatting_odds.py
+++ formatting_odds.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('./all_historical_odds.csv')
+print(len(df))
 
-df = df.drop_duplicates(subset=['Game ID', 'Point'])
+df = df.drop_duplicates(subset=['Game ID'])
+print(len(df))
 
 df.to_csv('all_historical_odds.csv', index=False)
